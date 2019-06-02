3c043dec8c4742d8a9e0d560b1d8726163e09d3f
You Matsumoto <youmts22@gmail.com>
2019-06-02 15:14:30 +0900

    Add confirmation from devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     email {"tester@example.com"}
     password {"password"}
+    confirmed_at {Time.current.yesterday}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "has a valid factory" do
-    expect(FactoryBot.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
+  end
+
+  it "is confirmed" do
+    user = create(:user)
+    expect(user.confirmed?).to be_truthy
   end
 end
