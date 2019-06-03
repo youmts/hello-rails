66a4ea0b71bf70ff990f2bb5694bd158bdba33f2
You Matsumoto <youmts22@gmail.com>
2019-06-03 22:52:48 +0900

    Add rspec for User.find_for_google

--- app/controllers/custom/devise/omniauth_callbacks_controller.rb
+++ app/controllers/custom/devise/omniauth_callbacks_controller.rb
@@ -2,13 +2,7 @@ class Custom::Devise::OmniauthCallbacksController < Devise::OmniauthCallbacksCon
   def google
     @user = User.find_for_google(request.env["omniauth.auth"])
 
-    # check whether a user exists.
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google.data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+    sign_in_and_redirect @user, :event => :authentication
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,47 @@ RSpec.describe User, type: :model do
     user = create(:user)
     expect(user.confirmed?).to be_truthy
   end
+
+  describe "User.find_for_google" do
+    context "already exists" do
+      it "returns the user" do
+        user = create(:user)
+        auth = OmniAuth::AuthHash.new(
+          {
+              info: {
+                  email: user.email
+              },
+              uid: "uid",
+              credentials: {
+                  token: "token"
+              }
+          }
+        )
+
+        expect {
+          expect(User.find_for_google(auth)).to eq user
+        }.to change{ User.count }.by(0)
+      end
+    end
+
+    context "not exists" do
+      it "returns new user" do
+        auth = OmniAuth::AuthHash.new(
+            {
+                info: {
+                    email: "new@example.com"
+                },
+                uid: "uid",
+                credentials: {
+                    token: "token"
+                }
+            }
+        )
+
+        expect {
+          expect(User.find_for_google(auth).email).to eq auth.info.email
+        }.to change{ User.count }.by(1)
+      end
+    end
+  end
 end
