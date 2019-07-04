0cf0ce95e87a848c1783dcf38258b1a836c1a12d
You Matsumoto <youmts22@gmail.com>
2019-07-04 23:05:00 +0900

    Add system spec for blog update/delete

--- spec/system/blogs_spec.rb
+++ spec/system/blogs_spec.rb
@@ -28,4 +28,32 @@ RSpec.describe 'Blogs', type: :system do
     expect(page).to have_current_path blog_path(Blog.last.id)
     expect(page).to have_content "Blog was successfully created."
   end
+
+  scenario 'Blogの編集が完了するとBlog閲覧画面に遷移すること' do
+    blog = create(:blog)
+    old_title = blog.title
+    new_title = old_title + "!"
+
+    visit edit_blog_path(blog)
+    fill_in 'Title', with: new_title
+    expect {
+      click_button 'Update Blog'
+    }.to change { Blog.find(blog.id).title }.from(old_title).to(new_title)
+
+    expect(page).to have_current_path blog_path(Blog.last.id)
+    expect(page).to have_content "Blog was successfully updated."
+  end
+
+  scenario 'Blogの削除が完了するとBlog閲覧画面に遷移すること' do
+    blog = create(:blog)
+
+    visit blogs_path
+    expect {
+      delete_link = find("a[data-method=\"delete\"][href=\"#{blog_path(blog)}\"]")
+      delete_link.click
+    }.to change(Blog, :count).by(-1)
+
+    expect(page).to have_current_path blogs_path
+    expect(page).to have_content "Blog was successfully destroyed."
+  end
 end
